c2fdc432055b86cc0ae67d1cce225b1afa698963
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-03-21 22:44:09 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-const elHeaderTogglerBtn = document.querySelector(".site-header__toggler")
-const elSitenav = document.querySelector(".sitenav")
+const elHeaderTogglerBtn = document.querySelector(".site-header__toggler");
+const elSitenav = document.querySelector(".sitenav");
 
-
-elHeaderTogglerBtn.addEventListener("click", evt => {
-  elSitenav.classList.toggle("sitenav--open")
-})
+elHeaderTogglerBtn.addEventListener("click", (evt) => {
+  elSitenav.classList.toggle("sitenav--open");
+});
